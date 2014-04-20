4324ae2a760c658586d3585c6aefe58883f4295d
Tristan B. Kildaire <tristankildaire@gmail.com>
2014-04-20 16:12:12 +0200

    Started a **Sunday project**.

---
+++ Searchy/Searchy/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        version_label.Text = My.Application.Info.Version.ToString
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Hide()
+    End Sub
+End Class
