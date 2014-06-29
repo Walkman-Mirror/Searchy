ac99feefef637ab1bc6b39315e70c4b976122508
░▒▓█│Walkman│█▓▒░ <matthewcarter17@gmail.com>
2014-06-30 00:18:19 +0200

    Spelling fix, removed redundant lines

--- Searchy/About.vb
+++ Searchy/About.vb
@@ -14,8 +14,7 @@
 '   You should have received a copy of the GNU General Public License
 '   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '
-' Contact the DeavmiOSS orginization by electronic mail: mailto:tristankildaire@gmail.com or mailto:walkman@hackermail.com (mailto:matthewcarter17@gmail.com)
-'   Contact the DeavmiOSS orginization by paper mail: <no info>
+'   Contact the DeavmiOSS organization by electronic mail: mailto:tristankildaire@gmail.com or mailto:walkman@hackermail.com (mailto:matthewcarter17@gmail.com)
 
 Public Class About
     Private Sub About_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
