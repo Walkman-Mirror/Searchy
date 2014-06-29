c80a5cff93347757e6cea3366ddc1bb458581600
░▒▓█│Walkman│█▓▒░ <matthewcarter17@gmail.com>
2014-06-29 20:57:34 +0200

    Adjusted dropdown

--- Searchy/Searchy.Designer.vb
+++ Searchy/Searchy.Designer.vb
@@ -47,14 +47,14 @@ Partial Class Searchy
         Me.NotificationContextMenuHide = New System.Windows.Forms.ToolStripMenuItem()
         Me.NotificationContextMenuExit = New System.Windows.Forms.ToolStripMenuItem()
         Me.timerKeyChecker = New System.Windows.Forms.Timer(Me.components)
-        Me.pnlTop.SuspendLayout()
-        CType(Me.imgLogo, System.ComponentModel.ISupportInitialize).BeginInit()
-        Me.NotificationContextMenu.SuspendLayout()
-        Me.SuspendLayout()
+        Me.pnlTop.SuspendLayout
+        CType(Me.imgLogo,System.ComponentModel.ISupportInitialize).BeginInit
+        Me.NotificationContextMenu.SuspendLayout
+        Me.SuspendLayout
         '
         'pnlTop
         '
-        Me.pnlTop.BackColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.pnlTop.BackColor = System.Drawing.Color.FromArgb(CType(CType(224,Byte),Integer), CType(CType(224,Byte),Integer), CType(CType(224,Byte),Integer))
         Me.pnlTop.Controls.Add(Me.lblVersionSmall)
         Me.pnlTop.Controls.Add(Me.imgLogo)
         Me.pnlTop.Controls.Add(Me.btnClose)
@@ -66,8 +66,8 @@ Partial Class Searchy
         '
         'lblVersionSmall
         '
-        Me.lblVersionSmall.AutoSize = True
-        Me.lblVersionSmall.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.lblVersionSmall.AutoSize = true
+        Me.lblVersionSmall.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64,Byte),Integer), CType(CType(64,Byte),Integer), CType(CType(64,Byte),Integer))
         Me.lblVersionSmall.Location = New System.Drawing.Point(171, 14)
         Me.lblVersionSmall.Name = "lblVersionSmall"
         Me.lblVersionSmall.Size = New System.Drawing.Size(41, 13)
@@ -82,7 +82,7 @@ Partial Class Searchy
         Me.imgLogo.Size = New System.Drawing.Size(62, 62)
         Me.imgLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
         Me.imgLogo.TabIndex = 3
-        Me.imgLogo.TabStop = False
+        Me.imgLogo.TabStop = false
         '
         'btnClose
         '
@@ -91,13 +91,13 @@ Partial Class Searchy
         Me.btnClose.Size = New System.Drawing.Size(62, 23)
         Me.btnClose.TabIndex = 2
         Me.btnClose.Text = "&X"
-        Me.btnClose.UseVisualStyleBackColor = True
+        Me.btnClose.UseVisualStyleBackColor = true
         '
         'lblProductName
         '
-        Me.lblProductName.AutoSize = True
-        Me.lblProductName.Font = New System.Drawing.Font("Microsoft Sans Serif", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblProductName.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.lblProductName.AutoSize = true
+        Me.lblProductName.Font = New System.Drawing.Font("Microsoft Sans Serif", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+        Me.lblProductName.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64,Byte),Integer), CType(CType(64,Byte),Integer), CType(CType(64,Byte),Integer))
         Me.lblProductName.Location = New System.Drawing.Point(67, 20)
         Me.lblProductName.Name = "lblProductName"
         Me.lblProductName.Size = New System.Drawing.Size(122, 33)
@@ -113,7 +113,7 @@ Partial Class Searchy
         '
         'lblTypeOfSearch
         '
-        Me.lblTypeOfSearch.AutoSize = True
+        Me.lblTypeOfSearch.AutoSize = true
         Me.lblTypeOfSearch.Location = New System.Drawing.Point(12, 99)
         Me.lblTypeOfSearch.Name = "lblTypeOfSearch"
         Me.lblTypeOfSearch.Size = New System.Drawing.Size(85, 13)
@@ -123,9 +123,11 @@ Partial Class Searchy
         'cbEngine
         '
         Me.cbEngine.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
-        Me.cbEngine.FormattingEnabled = True
+        Me.cbEngine.DropDownWidth = 110
+        Me.cbEngine.FormattingEnabled = true
         Me.cbEngine.Items.AddRange(New Object() {" == Web ==", "Google", "Yahoo", "Bing", "DuckDuckGo", " == Images ==", "Google Images", "Bing Images", " == Music ==", "SoundCloud", "Beatport", " == Video ==", "YouTube", "Dailymotion", "Google Videos", "Bing Videos", " == Social ==", "Reddit", " == Entertainment ==", "Newgrounds", " == Info ==", "Wikipedia", " == Products ==", "eBay", "Amazon", " == Code ==", "GitHub", "Bitbucket", "Stack Overflow", "Super User", "Stack Exchange"})
         Me.cbEngine.Location = New System.Drawing.Point(347, 114)
+        Me.cbEngine.MaxDropDownItems = 50
         Me.cbEngine.Name = "cbEngine"
         Me.cbEngine.Size = New System.Drawing.Size(79, 21)
         Me.cbEngine.TabIndex = 3
@@ -137,56 +139,56 @@ Partial Class Searchy
         Me.btnSearch.Size = New System.Drawing.Size(90, 23)
         Me.btnSearch.TabIndex = 4
         Me.btnSearch.Text = "&Search"
-        Me.btnSearch.UseVisualStyleBackColor = True
+        Me.btnSearch.UseVisualStyleBackColor = true
         '
         'NotificationIcon
         '
         Me.NotificationIcon.ContextMenuStrip = Me.NotificationContextMenu
-        Me.NotificationIcon.Icon = CType(resources.GetObject("NotificationIcon.Icon"), System.Drawing.Icon)
+        Me.NotificationIcon.Icon = CType(resources.GetObject("NotificationIcon.Icon"),System.Drawing.Icon)
         Me.NotificationIcon.Text = "Searchy"
-        Me.NotificationIcon.Visible = True
+        Me.NotificationIcon.Visible = true
         '
         'NotificationContextMenu
         '
         Me.NotificationContextMenu.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NotificationContextMenuSubmitFeedback, Me.NotificationContextMenuSeparator3, Me.NotificationContextMenuProjectSite, Me.NotificationContextMenuSourceCode, Me.NotificationContextMenuSeperator1, Me.NotificationContextMenuUpdate, Me.NotificationContextMenuAbout, Me.NotificationContextMenuSeperator2, Me.NotificationContextMenuPreferences, Me.NotificationContextMenuHide, Me.NotificationContextMenuExit})
         Me.NotificationContextMenu.Name = "ContextMenuStrip1"
-        Me.NotificationContextMenu.Size = New System.Drawing.Size(209, 198)
+        Me.NotificationContextMenu.Size = New System.Drawing.Size(220, 198)
         '
         'NotificationContextMenuSubmitFeedback
         '
         Me.NotificationContextMenuSubmitFeedback.Name = "NotificationContextMenuSubmitFeedback"
-        Me.NotificationContextMenuSubmitFeedback.Size = New System.Drawing.Size(208, 22)
+        Me.NotificationContextMenuSubmitFeedback.Size = New System.Drawing.Size(219, 22)
         Me.NotificationContextMenuSubmitFeedback.Text = "S&ubmit feedback"
         '
         'NotificationContextMenuSeparator3
         '
         Me.NotificationContextMenuSeparator3.Name = "NotificationContextMenuSeparator3"
-        Me.NotificationContextMenuSeparator3.Size = New System.Drawing.Size(205, 6)
+        Me.NotificationContextMenuSeparator3.Size = New System.Drawing.Size(216, 6)
         '
         'NotificationContextMenuProjectSite
         '
-        Me.NotificationContextMenuProjectSite.Image = CType(resources.GetObject("NotificationContextMenuProjectSite.Image"), System.Drawing.Image)
+        Me.NotificationContextMenuProjectSite.Image = CType(resources.GetObject("NotificationContextMenuProjectSite.Image"),System.Drawing.Image)
         Me.NotificationContextMenuProjectSite.Name = "NotificationContextMenuProjectSite"
-        Me.NotificationContextMenuProjectSite.Size = New System.Drawing.Size(208, 22)
+        Me.NotificationContextMenuProjectSite.Size = New System.Drawing.Size(219, 22)
         Me.NotificationContextMenuProjectSite.Text = "&Project site"
         '
         'NotificationContextMenuSourceCode
         '
-        Me.NotificationContextMenuSourceCode.Image = CType(resources.GetObject("NotificationContextMenuSourceCode.Image"), System.Drawing.Image)
+        Me.NotificationContextMenuSourceCode.Image = CType(resources.GetObject("NotificationContextMenuSourceCode.Image"),System.Drawing.Image)
         Me.NotificationContextMenuSourceCode.Name = "NotificationContextMenuSourceCode"
-        Me.NotificationContextMenuSourceCode.Size = New System.Drawing.Size(208, 22)
+        Me.NotificationContextMenuSourceCode.Size = New System.Drawing.Size(219, 22)
         Me.NotificationContextMenuSourceCode.Text = "&Source code"
         '
         'NotificationContextMenuSeperator1
         '
         Me.NotificationContextMenuSeperator1.Name = "NotificationContextMenuSeperator1"
-        Me.NotificationContextMenuSeperator1.Size = New System.Drawing.Size(205, 6)
+        Me.NotificationContextMenuSeperator1.Size = New System.Drawing.Size(216, 6)
         '
         'NotificationContextMenuUpdate
         '
         Me.NotificationContextMenuUpdate.Name = "NotificationContextMenuUpdate"
-        Me.NotificationContextMenuUpdate.ShortcutKeys = CType((System.Windows.Forms.Keys.Alt Or System.Windows.Forms.Keys.C), System.Windows.Forms.Keys)
-        Me.NotificationContextMenuUpdate.Size = New System.Drawing.Size(208, 22)
+        Me.NotificationContextMenuUpdate.ShortcutKeys = CType((System.Windows.Forms.Keys.Alt Or System.Windows.Forms.Keys.C),System.Windows.Forms.Keys)
+        Me.NotificationContextMenuUpdate.Size = New System.Drawing.Size(219, 22)
         Me.NotificationContextMenuUpdate.Text = "&Check for updates"
         '
         'NotificationContextMenuAbout
@@ -194,41 +196,38 @@ Partial Class Searchy
         Me.NotificationContextMenuAbout.Image = Global.Searchy.My.Resources.Resources._1402255166_susehelpcenter
         Me.NotificationContextMenuAbout.Name = "NotificationContextMenuAbout"
         Me.NotificationContextMenuAbout.ShortcutKeys = System.Windows.Forms.Keys.F1
-        Me.NotificationContextMenuAbout.Size = New System.Drawing.Size(208, 22)
+        Me.NotificationContextMenuAbout.Size = New System.Drawing.Size(219, 22)
         Me.NotificationContextMenuAbout.Text = "&About Searchy"
         '
         'NotificationContextMenuSeperator2
         '
         Me.NotificationContextMenuSeperator2.Name = "NotificationContextMenuSeperator2"
-        Me.NotificationContextMenuSeperator2.Size = New System.Drawing.Size(205, 6)
+        Me.NotificationContextMenuSeperator2.Size = New System.Drawing.Size(216, 6)
         '
         'NotificationContextMenuPreferences
         '
         Me.NotificationContextMenuPreferences.Image = Global.Searchy.My.Resources.Resources._1402255170_package_settings
         Me.NotificationContextMenuPreferences.Name = "NotificationContextMenuPreferences"
-        Me.NotificationContextMenuPreferences.Size = New System.Drawing.Size(208, 22)
+        Me.NotificationContextMenuPreferences.Size = New System.Drawing.Size(219, 22)
         Me.NotificationContextMenuPreferences.Text = "&Preferences"
         '
         'NotificationContextMenuHide
         '
         Me.NotificationContextMenuHide.Name = "NotificationContextMenuHide"
-        Me.NotificationContextMenuHide.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.H), System.Windows.Forms.Keys)
-        Me.NotificationContextMenuHide.Size = New System.Drawing.Size(208, 22)
+        Me.NotificationContextMenuHide.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.H),System.Windows.Forms.Keys)
+        Me.NotificationContextMenuHide.Size = New System.Drawing.Size(219, 22)
         Me.NotificationContextMenuHide.Text = "&Hide"
         '
         'NotificationContextMenuExit
         '
         Me.NotificationContextMenuExit.Name = "NotificationContextMenuExit"
-        Me.NotificationContextMenuExit.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.E), System.Windows.Forms.Keys)
-        Me.NotificationContextMenuExit.Size = New System.Drawing.Size(208, 22)
+        Me.NotificationContextMenuExit.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.E),System.Windows.Forms.Keys)
+        Me.NotificationContextMenuExit.Size = New System.Drawing.Size(219, 22)
         Me.NotificationContextMenuExit.Text = "&Exit"
-        '
-        'timerKeyChecker
-        '
         '
         'Searchy
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6!, 13!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(534, 172)
         Me.Controls.Add(Me.btnSearch)
@@ -237,18 +236,17 @@ Partial Class Searchy
         Me.Controls.Add(Me.txtQuery)
         Me.Controls.Add(Me.pnlTop)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
-        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Icon = CType(resources.GetObject("$this.Icon"),System.Drawing.Icon)
         Me.Name = "Searchy"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Searchy"
-        Me.TopMost = True
-        Me.pnlTop.ResumeLayout(False)
-        Me.pnlTop.PerformLayout()
-        CType(Me.imgLogo, System.ComponentModel.ISupportInitialize).EndInit()
-        Me.NotificationContextMenu.ResumeLayout(False)
-        Me.ResumeLayout(False)
-        Me.PerformLayout()
-
+        Me.TopMost = true
+        Me.pnlTop.ResumeLayout(false)
+        Me.pnlTop.PerformLayout
+        CType(Me.imgLogo,System.ComponentModel.ISupportInitialize).EndInit
+        Me.NotificationContextMenu.ResumeLayout(false)
+        Me.ResumeLayout(false)
+        Me.PerformLayout
     End Sub
     Friend WithEvents pnlTop As System.Windows.Forms.Panel
     Friend WithEvents lblProductName As System.Windows.Forms.Label
